bc3b2a1e54ad987cfbcc7dbe83d45bba8c79684e
jaehee <jaehee23@naver.com>
2020-03-16 19:44:32 +0900

    jaehee1

--- src/components/accordion.vue
+++ src/components/accordion.vue
@@ -14,6 +14,7 @@
 <script lang="ts">
 import {Vue, Component, Mixins} from 'vue-property-decorator';
 import toggle from '@/components/toggle.vue';
+// jaehee1
 @Component({
     components: {
         toggle,
